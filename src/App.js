44057e03520e65d1d0a2c23a29c44d3024c3d37c
Ayano Taki <ayanobayern2@gmail.com>
2020-09-18 03:37:15 +0900

    カウント機能実装

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import './App.css';
 import Controls from './components/Controls';
 import Timer from './components/Timer';
 
 const App = () => {
-  const timer = "時間"
-  const buttonList = [
-    {name: "Start", id: "start"},
-    {name: "Stop", id: "stop"},
-    {name: "Reset", id: "reset"}
-  ]
+  const [ timer, setTimer ] = useState(0)
+
   return (
     <div className="container">
       <Timer timer={timer}/>
-      <div className="controls">
-        {buttonList.map(button => 
-          (
-            <Controls name={button.name} id={button.id} key={button.id}/>
-          )
-        )}
-      </div>
+      <Controls setTimer={setTimer}/>
     </div>
   );
 }
--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React,{ useState } from 'react'
 
 const Controls = (props) => {
+    const [startDisabled, setStartDisabled] = useState(false)
+    const [stopDisabled, setStopDisabled] = useState(true)
+    const [resetDisabled, setResetDisabled] = useState(true)
+
+    const countUp = () => {
+        props.setTimer(prevTimer => prevTimer + 1)
+        setTimeout(() => {
+            countUp();
+        },1000)
+    }
+    const handleStart = () => {
+        setStartDisabled(true)
+        setStopDisabled(false)
+        setResetDisabled(true)
+        countUp();
+    }
+    const handleStop = () => {
+        setStartDisabled(false)
+        setStopDisabled(true)
+        setResetDisabled(false)
+    }
+    const handleReset = () => {
+        setStartDisabled(false)
+        setStopDisabled(true)
+        setResetDisabled(true)
+        props.setTimer("RESET!")
+        console.log("RESET")
+    }
 
     return (
-    <button className="btn">{props.name}</button>
+        <div className="controls">
+            <button 
+                id="start" name="start" className="btn" 
+                disabled={startDisabled} onClick={handleStart}
+            >
+                Start
+            </button>
+            <button
+                id="stop" name="stop"  className="btn"
+                disabled={stopDisabled} onClick={handleStop}
+            >
+                Stop
+            </button>
+            <button
+                id="reset" name="reset" className="btn"
+                disabled={resetDisabled} onClick={handleReset}
+            >
+                Reset
+            </button>
+        </div>
     )
 }
 
